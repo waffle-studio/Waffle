83f93f3bc9fa186b153d961c48398278f9b3b607
Shunki Takami <tkmnet@users.noreply.github.com>
2020-07-14 04:07:01 +0900

    ResourceFileキャッシュ化

--- src/main/java/jp/tkms/waffle/data/util/ResourceFile.java
+++ src/main/java/jp/tkms/waffle/data/util/ResourceFile.java
@@ -1,14 +1,25 @@
 package jp.tkms.waffle.data.util;
 
+import jp.tkms.waffle.data.Job;
+import jp.tkms.waffle.data.log.ErrorLogMessage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
 public class ResourceFile {
   private static ResourceFile staticInstance = new ResourceFile();
 
+  private static final HashMap<String, String> instanceMap = new HashMap<>();
+
   public static String getContents(String path) {
+    String candidate = instanceMap.get(path);
+    if (candidate != null) {
+      return candidate;
+    }
+
     StringBuilder contents = new StringBuilder();
     InputStream in = staticInstance.getClass().getResourceAsStream(path);
     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
@@ -20,8 +31,11 @@ public class ResourceFile {
       }
       reader.close();
     } catch (IOException e) {
-      e.printStackTrace();
+      ErrorLogMessage.issue(e);
+      return "";
     }
+
+    instanceMap.put(path, contents.toString());
     return contents.toString();
   }
 
