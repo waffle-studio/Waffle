a9b4d4cebeb4ce60d8f65c5d956e6f0ef4676868
Shunki Takami <tkmnet@users.noreply.github.com>
2020-07-30 09:26:22 +0900

    vjr追加

---
+++ sub_project/waffle-virtual-job-runner/src/main/java/jp/tkms/waffle/sub/vjr/Main.java
@@ -0,0 +1,11 @@
+package jp.tkms.waffle.sub.vjr;
+
+public class Main {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
