bb895804a343d31fbed82db244ff26c361c50bf7
Shunki Takami <tkmnet@users.noreply.github.com>
2020-08-13 07:24:57 +0900

    Fileロック方法変更

--- src/main/java/jp/tkms/waffle/data/util/FileBuffer.java
+++ src/main/java/jp/tkms/waffle/data/util/FileBuffer.java
@@ -12,206 +12,60 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class FileBuffer extends Thread {
-  private Path path;
-  private String contents;
-  private boolean isReadMode;
-  private boolean alive = true;
-  private long timestamp;
-
-  private static ExecutorService threadPool = new ThreadPoolExecutor(0, Runtime.getRuntime().availableProcessors(), 60L, TimeUnit.SECONDS, new LinkedBlockingQueue());
-  private static Lock bufferLock = new ReentrantLock();
-  private static boolean flushFlag = false;
-  private static ConcurrentHashMap<Path, FileBuffer> map = new ConcurrentHashMap<>();
-  private static ConcurrentLinkedQueue<FileBuffer> queue = new ConcurrentLinkedQueue<>();
-  private static HashSet<FileBuffer> runningSet = new HashSet<>();
+public class FileBuffer {
+  private static ConcurrentHashMap<Path, Object> map = new ConcurrentHashMap<>();
 
   public static void write(Path path, String contents) {
-    try {
-      Path directoryPath = path.getParent();
-      if (!Files.exists(directoryPath)) {
-        Files.createDirectories(directoryPath);
-      }
+    Object locker = null;
+    path = path.toAbsolutePath();
 
-      FileWriter filewriter = new FileWriter(path.toFile());
-      filewriter.write(contents);
-      filewriter.close();
-    } catch (IOException e) {
-      ErrorLogMessage.issue(e);
-    }
-    /*
-    checkFlushFlag();
-    runLifeCycle();
     synchronized (map) {
-      Path absolutePath = path.toAbsolutePath();
-      FileBuffer fileBuffer = map.get(absolutePath);
-      if (fileBuffer != null && fileBuffer.alive) {
-        fileBuffer.setContents(contents);
-        if (fileBuffer.isReadMode) {
-          threadPool.submit(fileBuffer);
-          fileBuffer.isReadMode = false;
-        }
-        return;
-      }
-      fileBuffer = new FileBuffer(absolutePath, contents);
-      map.put(absolutePath, fileBuffer);
-      queue.offer(fileBuffer);
-      if (Files.exists(absolutePath)) {
-        threadPool.submit(fileBuffer);
-      } else {
-        fileBuffer.writeToDisk();
-        fileBuffer.isReadMode = true;
+      locker = map.get(path);
+      if (locker == null) {
+        locker = new Object();
+        map.put(path, locker);
       }
     }
 
-     */
+    synchronized (locker) {
+      try {
+        Path directoryPath = path.getParent();
+        if (!Files.exists(directoryPath)) {
+          Files.createDirectories(directoryPath);
+        }
+
+        FileWriter filewriter = new FileWriter(path.toFile());
+        filewriter.write(contents);
+        filewriter.close();
+      } catch (IOException e) {
+        ErrorLogMessage.issue(e);
+      }
+
+      map.remove(path);
+    }
   }
 
   public static String read(Path path) {
-    try {
-      return new String(Files.readAllBytes(path));
-    } catch (IOException e) {
-      return "";
-    }
-    /*
-    checkFlushFlag();
-    runLifeCycle();
-    FileBuffer fileBuffer = null;
+    Object locker = null;
+    path = path.toAbsolutePath();
+
     synchronized (map) {
-      Path absolutePath = path.toAbsolutePath();
-      fileBuffer = map.get(absolutePath);
-      if (fileBuffer == null) {
-        fileBuffer = new FileBuffer(absolutePath);
-        map.put(absolutePath, fileBuffer);
-        queue.offer(fileBuffer);
+      locker = map.get(path);
+      if (locker == null) {
+        locker = new Object();
+        map.put(path, locker);
       }
     }
-    return fileBuffer.getContents();
 
-     */
-  }
-
-  public static void flush() {
-    bufferLock.lock();
-    try {
-      flushFlag = true;
-      synchronized (map) {
-        for (FileBuffer fileBuffer : queue) {
-          if (!fileBuffer.isReadMode) {
-            fileBuffer.interrupt();
-          }
-        }
-        map.clear();
-        queue.clear();
-      }
-      int runningCount = 0;
-      do {
-        synchronized (runningSet) {
-          runningCount = runningSet.size();
-        }
-      } while (runningCount > 0);
-      flushFlag = false;
-    } finally {
-      bufferLock.unlock();
-    }
-  }
-
-  public static void shutdown() {
-    try {
-      flush();
-      threadPool.shutdown();
-      threadPool.awaitTermination(7, TimeUnit.DAYS);
-    } catch (InterruptedException e) {
-      ErrorLogMessage.issue(e);
-    }
-  }
-
-  private static void checkFlushFlag() {
-    bufferLock.lock();
-    bufferLock.unlock();
-  }
-
-  private static void runLifeCycle() {
-    synchronized (map) {
-      while (!queue.isEmpty() && queue.peek().isReadMode && queue.peek().timestamp + 1000 > System.currentTimeMillis()) {
-        FileBuffer fileBuffer = queue.poll();
-        map.remove(fileBuffer.path);
-      }
-    }
-  }
-
-  private FileBuffer(Path path, String contents) {
-    super(FileBuffer.class.getSimpleName());
-    this.path = path;
-    this.contents = contents;
-    this.isReadMode = false;
-    this.timestamp = System.currentTimeMillis();
-  }
-
-  private FileBuffer(Path path) {
-    super(FileBuffer.class.getSimpleName());
-    this.path = path;
-    try {
-      this.contents = new String(Files.readAllBytes(path));
-    } catch (IOException e) {
-      this.contents = "";
-    }
-    this.isReadMode = true;
-    this.timestamp = System.currentTimeMillis();
-  }
-
-  private String getContents() {
-    synchronized (this) {
-      return contents;
-    }
-  }
-
-  private void setContents(String contents) {
-    synchronized (this) {
-      this.contents = contents;
-    }
-    synchronized (map) {
-      this.timestamp = System.currentTimeMillis();
-      queue.remove(this);
-      queue.add(this);
-    }
-  }
-
-  private void writeToDisk() {
-    try {
-      Path directoryPath = path.getParent();
-      if (!Files.exists(directoryPath)) {
-        Files.createDirectories(directoryPath);
-      }
-
-      FileWriter filewriter = new FileWriter(path.toFile());
-      filewriter.write(contents);
-      filewriter.close();
-    } catch (IOException e) {
-      ErrorLogMessage.issue(e);
-    }
-  }
-
-  @Override
-  public void run() {
-    synchronized (runningSet) {
-      runningSet.add(this);
-    }
-    while (timestamp + 1000 > System.currentTimeMillis() && !isInterrupted()) {
+    String contents;
+    synchronized (locker) {
       try {
-        Thread.sleep(200);
-      } catch (InterruptedException e) { }
-    }
-    if (!flushFlag) {
-      synchronized (map) {
-        map.remove(path);
-        queue.remove(this);
-        alive = false;
+        contents = new String(Files.readAllBytes(path));
+      } catch (IOException e) {
+        contents = "";
       }
+      map.remove(path);
     }
-    writeToDisk();
-    synchronized (runningSet) {
-      runningSet.remove(this);
-    }
+    return contents;
   }
 }
