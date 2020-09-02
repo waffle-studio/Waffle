5c4fec24ea5febbe478cbe8eaa2860a6161f237d
Shunki Takami <tkmnet@users.noreply.github.com>
2020-09-02 21:32:41 +0900

    一時保存

--- sub_project/waffle-virtual-job-executor/src/main/java/jp/tkms/waffle/sub/vje/Main.java
+++ sub_project/waffle-virtual-job-executor/src/main/java/jp/tkms/waffle/sub/vje/Main.java
@@ -1,6 +1,10 @@
 package jp.tkms.waffle.sub.vje;
 
 public class Main {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println("invalid arguments");
+      System.exit(1);
     }
+  }
 }
