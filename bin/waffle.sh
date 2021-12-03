02ac7602551f0bb7010859c9e8099fedc96b4e72
Shunki Takami <s-takami@tkms.jp>
2021-12-04 07:36:29 +0900

    waffle.shにJRE追加

--- bin/waffle.sh
+++ bin/waffle.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+JRE_URL="https://files.tkms.jp/waffle-jre.tar.gz"
 JAR_URL="https://desk.tkms.jp/resource/WjjMKKgC2MdI4GpepkzGOy4yQ3dWEOxM/waffle-all.jar"
+JAVA_OPT="--add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
+
 WAFFLE_INTERNAL="WAFFLE/.INTERNAL"
 START_FLAG=${WAFFLE_INTERNAL}/AUTO_START
 mkdir -p $WAFFLE_INTERNAL
@@ -23,7 +26,33 @@ if [ ! -e "$JAR_FILE" ];then
   fi
 fi
 
-JAVA="java"
+JRE_FILE="${WAFFLE_INTERNAL}/waffle-jre.tar.gz"
+JRE_DIR="${WAFFLE_INTERNAL}/waffle-jre"
+if [ ! -e "$JRE_DIR" ];then
+  if [ -e "$JRE_FILE" ];then
+    sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+  else
+    if [ -e "waffle-jre.tar.gz" ];then
+      cp waffle-jre.tar.gz $JRE_FILE
+      sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+    else
+      if which curl >/dev/null 2>&1; then
+        curl -o "$JRE_FILE" "$JRE_URL"
+        sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+      else
+        if which wget >/dev/null 2>&1; then
+          wget -O "$JRE_FILE" "$JRE_URL"
+          sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+        else
+          echo "Please download manually waffle-jre.tar.gz because cURL and Wget are not available."
+          exit 1;
+        fi
+      fi
+    fi
+  fi
+fi
+
+JAVA="${JRE_DIR}/bin/java"
 if [ -n "${JAVA_HOME}" ];then
   JAVA="${JAVA_HOME}/bin/${JAVA}"
 fi
@@ -31,6 +60,6 @@ fi
 touch $START_FLAG
 while [ -e "$START_FLAG" ];do
   rm -f $START_FLAG
-  ${JAVA} -jar ${JAR_FILE}
+  ${JAVA} ${JAVA_OPT} -jar ${JAR_FILE}
 done
 
