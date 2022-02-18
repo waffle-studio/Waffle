76503256d9510833197ef47a4635eec231d54076
Shunki Takami <s-takami@tkms.jp>
2022-02-18 21:50:26 +0900

    Wrapperでキャンセルができない問題に対処

--- src/main/java/jp/tkms/waffle/communicator/AbstractSubmitterWrapper.java
+++ src/main/java/jp/tkms/waffle/communicator/AbstractSubmitterWrapper.java
@@ -1,9 +1,19 @@
 package jp.tkms.waffle.communicator;
 
 import jp.tkms.waffle.data.computer.Computer;
+import jp.tkms.waffle.data.internal.task.AbstractTask;
+import jp.tkms.waffle.data.util.State;
+import jp.tkms.waffle.exception.FailedToControlRemoteException;
+import jp.tkms.waffle.exception.RunNotFoundException;
+import jp.tkms.waffle.sub.servant.Envelope;
 
 public abstract class AbstractSubmitterWrapper extends AbstractSubmitter {
   public AbstractSubmitterWrapper(Computer computer) {
     super(computer);
+  }
+
+  @Override
+  public void cancel(Envelope envelope, AbstractTask job) throws RunNotFoundException, FailedToControlRemoteException {
+    job.setState(State.Canceled);
   }
 }
