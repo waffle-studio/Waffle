212fda49ec2fd947c7f8bae0af9f94023ef0b3fe
Shunki Takami <s-takami@tkms.jp>
2022-11-02 19:39:53 +0900

    update make_jre

---
+++ make_jre.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+OUT="build/waffle-jre"
+cd $(dirname $0)
+./gradlew build
+MODS=$(jdeps -s build/libs/waffle-all.jar | sed -e 's/.*-> //' | grep -v "not found" | grep -v "java.base" |awk 'BEGIN{s="java.base"}(""!=$1){s=s "," $1}END{print s}')
+rm -rf $OUT
+jlink --add-modules $MODS --compress=2 --no-header-files --no-man-pages --output $OUT
+cd build
+tar cvf waffle-jre.tar.gz waffle-jre
+
