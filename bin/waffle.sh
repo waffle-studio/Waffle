183b6c87b9635e6906ccb6a5a7d0b4a76ca14d77
Shunki Takami <s-takami@tkms.jp>
2022-11-07 15:07:21 +0900

    add update option to waffle.sh

--- bin/waffle.sh
+++ bin/waffle.sh
@@ -9,6 +9,17 @@ START_FLAG=${WAFFLE_INTERNAL}/AUTO_START
 mkdir -p $WAFFLE_INTERNAL
 
 JAR_FILE="${WAFFLE_INTERNAL}/waffle-all.jar"
+JRE_FILE="${WAFFLE_INTERNAL}/waffle-jre.tar.gz"
+JRE_DIR="${WAFFLE_INTERNAL}/waffle-jre"
+
+if [ "$1" = "u" ];then
+  rm $JAR_FILE >/dev/null 2>&1
+elif [ "$1" = "U" ];then
+  rm $JAR_FILE >/dev/null 2>&1
+  rm $JRE_FILE >/dev/null 2>&1
+  rm -rf $JRE_DIR >/dev/null 2>&1
+fi
+
 if [ ! -e "$JAR_FILE" ];then
   if [ -e "waffle-all.jar" ];then
     cp waffle-all.jar $JAR_FILE
@@ -26,8 +37,6 @@ if [ ! -e "$JAR_FILE" ];then
   fi
 fi
 
-JRE_FILE="${WAFFLE_INTERNAL}/waffle-jre.tar.gz"
-JRE_DIR="${WAFFLE_INTERNAL}/waffle-jre"
 if [ ! -e "$JRE_DIR/bin/java" ];then
   if [ -e "$JRE_FILE" ];then
     sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
