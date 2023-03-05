2a929bef0609d7449e54f1c5c23e9cc3df027e67
Shunki Takami <s-takami@tkms.jp>
2023-03-05 17:52:24 +0900

    update updating process

--- bin/waffle.sh
+++ bin/waffle.sh
@@ -4,8 +4,11 @@ JRE_URL="https://files.tkms.jp/waffle-jre.tar.gz"
 JAR_URL="https://desk.tkms.jp/resource/WjjMKKgC2MdI4GpepkzGOy4yQ3dWEOxM/waffle-all.jar"
 JAVA_OPT="--add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED"
 
+export WAFFLE_SH="$0"
+
 WAFFLE_INTERNAL="WAFFLE/.INTERNAL"
 START_FLAG=${WAFFLE_INTERNAL}/AUTO_START
+REMOVE_FLAG=${WAFFLE_INTERNAL}/REMOVE_JAR
 mkdir -p $WAFFLE_INTERNAL
 
 JAR_FILE="${WAFFLE_INTERNAL}/waffle-all.jar"
@@ -20,47 +23,6 @@ elif [ "$1" = "-U" ];then
   rm -rf $JRE_DIR >/dev/null 2>&1
 fi
 
-if [ ! -e "$JAR_FILE" ];then
-  if [ -e "waffle-all.jar" ];then
-    cp waffle-all.jar $JAR_FILE
-  else
-    if which curl >/dev/null 2>&1; then
-      curl -o "$JAR_FILE" "$JAR_URL"
-    else
-      if which wget >/dev/null 2>&1; then
-        wget -O "$JAR_FILE" "$JAR_URL"
-      else
-        echo "Please download manually waffle-all.jar because cURL and Wget are not available."
-        exit 1;
-      fi
-    fi
-  fi
-fi
-
-if [ ! -e "$JRE_DIR/bin/java" ];then
-  if [ -e "$JRE_FILE" ];then
-    sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
-  else
-    if [ -e "waffle-jre.tar.gz" ];then
-      cp waffle-jre.tar.gz $JRE_FILE
-      sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
-    else
-      if which curl >/dev/null 2>&1; then
-        curl -o "$JRE_FILE" "$JRE_URL"
-        sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
-      else
-        if which wget >/dev/null 2>&1; then
-          wget -O "$JRE_FILE" "$JRE_URL"
-          sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
-        else
-          echo "Please download manually waffle-jre.tar.gz because cURL and Wget are not available."
-          exit 1;
-        fi
-      fi
-    fi
-  fi
-fi
-
 if [ -n "${JAVA_HOME}" ];then
   JAVA="${JAVA_HOME}/bin/java"
 else
@@ -73,7 +35,54 @@ fi
 
 touch $START_FLAG
 while [ -e "$START_FLAG" ];do
+
+  if [ ! -e "$JAR_FILE" ];then
+    if [ -e "waffle-all.jar" ];then
+      cp waffle-all.jar $JAR_FILE
+    else
+      if which curl >/dev/null 2>&1; then
+        curl -o "$JAR_FILE" "$JAR_URL"
+      else
+        if which wget >/dev/null 2>&1; then
+          wget -O "$JAR_FILE" "$JAR_URL"
+        else
+          echo "Please download manually waffle-all.jar because cURL and Wget are not available."
+          exit 1;
+        fi
+      fi
+    fi
+  fi
+
+  if [ ! -e "$JRE_DIR/bin/java" ];then
+    if [ -e "$JRE_FILE" ];then
+      sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+    else
+      if [ -e "waffle-jre.tar.gz" ];then
+        cp waffle-jre.tar.gz $JRE_FILE
+        sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+      else
+        if which curl >/dev/null 2>&1; then
+          curl -o "$JRE_FILE" "$JRE_URL"
+          sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+        else
+          if which wget >/dev/null 2>&1; then
+            wget -O "$JRE_FILE" "$JRE_URL"
+            sh -c "cd ${WAFFLE_INTERNAL} && tar xf waffle-jre.tar.gz"
+          else
+            echo "Please download manually waffle-jre.tar.gz because cURL and Wget are not available."
+            exit 1;
+          fi
+        fi
+      fi
+    fi
+  fi
+
   rm -f $START_FLAG
   ${JAVA} ${JAVA_OPT} -jar ${JAR_FILE}
+
+  if [ -e "$REMOVE_FLAG" ];then
+    rm $REMOVE_FLAG >/dev/null 2>&1
+    rm $JAR_FILE >/dev/null 2>&1
+  fi
 done
 
