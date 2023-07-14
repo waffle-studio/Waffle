29c1720c9d2322555055965fa5d77d9f9925d1be
Shunki Takami <s-takami@tkms.jp>
2023-07-15 01:37:34 +0900

    fix retry func

--- sub_project/waffle-servant/src/main/java/jp/tkms/waffle/sub/servant/processor/CheckJobIdRequestProcessor.java
+++ sub_project/waffle-servant/src/main/java/jp/tkms/waffle/sub/servant/processor/CheckJobIdRequestProcessor.java
@@ -5,17 +5,23 @@ import jp.tkms.waffle.sub.servant.Envelope;
 import jp.tkms.waffle.sub.servant.message.request.CheckJobIdMessage;
 import jp.tkms.waffle.sub.servant.message.response.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
 
 public class CheckJobIdRequestProcessor extends RequestProcessor<CheckJobIdMessage> {
+  public static final int MAX_REMOVING_RETRYING = 100;
   protected CheckJobIdRequestProcessor() {
   }
 
   @Override
   protected void processIfMessagesExist(Path baseDirectory, Envelope request, Envelope response, ArrayList<CheckJobIdMessage> messageList) throws ClassNotFoundException, IOException {
+    ArrayList<Path> removingList = new ArrayList<>();
+
     messageList.stream().parallel().forEach(message-> {
       Path workingDirectory = baseDirectory.resolve(message.getWorkingDirectory()).toAbsolutePath().normalize();
       String jobId = "";
@@ -32,10 +38,31 @@ public class CheckJobIdRequestProcessor extends RequestProcessor<CheckJobIdMessa
 
       jobId = jobId.trim();
       if ("".equals(jobId)) {
+        removingList.add(workingDirectory);
         response.add(new RequestRepreparingMessage(message));
       } else {
         response.add(new UpdateJobIdMessage(message, jobId, workingDirectory));
       }
     });
+
+    if (!removingList.isEmpty()) {
+      try {
+        TimeUnit.SECONDS.sleep(Constants.WATCHDOG_INTERVAL);
+      } catch (InterruptedException e) {
+        //NOP
+      }
+      removingList.stream().parallel().forEach(path -> {
+        if (Files.exists(path)) {
+          for (int count = 0; count < MAX_REMOVING_RETRYING; count += 1) {
+            try {
+              Files.walk(path).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+            } catch (IOException e) {
+              continue;
+            }
+            break;
+          }
+        }
+      });
+    }
   }
 }
